49e4e85c9d56934009f1d283b4eaadb40b577888
RosarioFirenze <121192643+RosarioFirenze@users.noreply.github.com>
2022-12-27 14:09:49 +0100

    rev27-12

---
+++ script.js
@@ -0,0 +1,34 @@
+// Eventi e Funzioni 
+const eve = document.getElementById('myBtn');
+eve.addEventListener('click',aggiungiElemento);
+
+
+function aggiungiElemento(){
+let text = document.getElementById('myInput').value;
+let mylist = document.getElementById('myList');
+let parag = document.createElement('p')
+mylist.appendChild(parag);
+parag.innerHTML = text;
+
+}
+
+const eve2 = document.getElementById('myBtn2');
+eve2.addEventListener('click',eliminaTutto)
+
+function eliminaTutto(){
+    let mylist = document.getElementById('myList');
+    while(mylist.firstChild){
+        mylist.removeChild(mylist.firstChild)
+    }
+}
+const eve3 = document.getElementById('myBtn3');
+eve3.addEventListener('click',eliminaElemento)
+
+function eliminaElemento(){
+    let mylist = document.getElementById('myList');
+        if(mylist.hasChildNodes){
+        mylist.removeChild(mylist.firstChild)
+        }
+    
+}
+
