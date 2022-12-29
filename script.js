8771ad4b5a67bd6e8071ef9e24d7ccd1d18fddff
RosarioFirenze <121192643+RosarioFirenze@users.noreply.github.com>
2022-12-29 17:35:52 +0100

    up29/12

--- script.js
+++ script.js
@@ -2,23 +2,22 @@
 const eve = document.getElementById('myBtn');
 eve.addEventListener('click',aggiungiElemento);
 
-
 function aggiungiElemento(){
 let text = document.getElementById('myInput').value;
 let mylist = document.getElementById('myList');
-let parag = document.createElement('p')
+let parag = document.createElement('p');
 mylist.appendChild(parag);
 parag.innerHTML = text;
-
-}
+document.getElementById("myInput").value = ""
+};
 
 const eve2 = document.getElementById('myBtn2');
 eve2.addEventListener('click',eliminaTutto)
 
 function eliminaTutto(){
     let mylist = document.getElementById('myList');
-    while(mylist.firstChild){
-        mylist.removeChild(mylist.firstChild)
+    while(mylist.firstChild){   
+        mylist.removeChild(mylist.firstChild)  /* questa parte non l'ho capita*/
     }
 }
 const eve3 = document.getElementById('myBtn3');
@@ -27,8 +26,7 @@ eve3.addEventListener('click',eliminaElemento)
 function eliminaElemento(){
     let mylist = document.getElementById('myList');
         if(mylist.hasChildNodes){
-        mylist.removeChild(mylist.firstChild)
-        }
-    
+        mylist.removeChild(mylist.lastChild)
+        }    
 }
 
